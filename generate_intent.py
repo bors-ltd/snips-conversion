2645036706a4a9ccd10bc8b6ebc10b7d8fdcee34
Bors, Ltd <github@bors-ltd.fr>
2019-03-12 19:32:55 +0100

    reorder intents with more examples

--- generate_intent.py
+++ generate_intent.py
@@ -5,19 +5,26 @@ import re
 
 
 SENTENCES = (
-    "[17](quantity) [miles](source_unit), ça fait beaucoup ?",
-    "Je mesure [170](quantity) [centimètres](source_unit), je mesure combien en [pieds](dest_unit) ?",
-    "Si la vitesse est limitée à [60](quantity) [miles par heure](source_unit), à combien je peux rouler en [kilomètre heure](dest_unit) ?",
-    "Si je roule à [80](quantity) [kilomètres à l'heure](source_unit), je roule à combien en [miles par heure](dest_unit) ?",
-    "Si je fais [deux](quantity) [mètres](source_unit), ça fait combien en [pieds](dest_unit) ?",
+    "[17](quantity) [pieds](source_unit), ça fait combien ?",
+    "[80](quantity) [miles](source_unit), ça fait beaucoup ?",
+    "[100](quantity) [degrés fahrenheit](source_unit), c'est chaud ?",
+    "Convertis [2](quantity) [livres](source_unit)",
+    "Convertis [1](quantity) [mètre](source_unit) en [pouces](dest_unit)",
+    "La conversion de [3](quantity) [gallons](source_unit) en [litres](dest_unit) ?",
+    "La conversion de [9](quantity) [pintes](source_unit) en [litres](dest_unit), s'il te plaît",
     "[537](quantity) [minutes](source_unit), ça fait combien [d'heures](dest_unit) ?",
-    "[Quarante](quantity) [yards](source_unit) en [miles](dest_unit), ça fait combien ?",
-    "La conversion de [trois](quantity) [gallons](source_unit) en [litres](dest_unit) ?",
+    "[40](quantity) [yards](source_unit) en [miles](dest_unit), ça fait combien ?",
     "[7](quantity) [livres](source_unit) en [grammes](dest_unit) ?",
     "Donne-moi [20](quantity) [degrés](source_unit) en [fahrenheit](dest_unit)",
-    "Ça fait combien [quatre](quantity) [miles](source_unit) en [kilomètres](dest_unit) ?",
-    "Combien font [deux](quantity) [pouces](source_unit) ?",
-    "Convertis [un](quantity) [mètre](source_unit) en [pouces](dest_unit)",
+    "Ça fait combien [4](quantity) [miles](source_unit) en [kilomètres](dest_unit) ?",
+    "Il y a combien de [grammes](dest_unit) dans [1](quantity) [livre](source_unit) ?",
+    "Il y a combien de [jours](dest_unit) dans [1](quantity) [année sidérale](source_unit) ?",
+    "[20](quantity) [nœuds](source_unit), ça souffle fort ?",
+    "Je mesure [170](quantity) [centimètres](source_unit), je mesure combien en [pieds](dest_unit) ?",
+    "Si je fais [2](quantity) [mètres](source_unit), ça fait combien en [pieds](dest_unit) ?",
+    "Si la vitesse est limitée à [60](quantity) [miles par heure](source_unit), à combien je peux rouler en [kilomètre heure](dest_unit) ?",
+    "Si je roule à [80](quantity) [kilomètres à l'heure](source_unit), je roule à combien en [miles par heure](dest_unit) ?",
+    # "Quelle est la vitesse de la lumière ?",
 )
 
 SOURCE_PATTERN = re.compile(r"\[([\w\s']+)\]\(source_unit\)", flags=re.UNICODE)
@@ -26,25 +33,31 @@ DEST_PATTERN = re.compile(r"\[([\w\s']+)\]\(dest_unit\)", flags=re.UNICODE)
 # A mix of abbreviations sometimes Snips is giving without asking
 # and alternative expressions we would say
 ALTERNATIVES = {
-    "miles": "miles",  # or "mailles"?
+    "année sidérale": "année sidérale",
     "centimètres": "cm",
-    "pieds": "pieds",
-    "miles par heure": "miles à l'heure",
+    "degrés": "degrés",
+    "degrés fahrenheit": "fahrenheit",
+    "d'heures": "en heures",
+    "fahrenheit": "degrés fahrenheit",
+    "gallons": "gallons",  # "galons"?
+    "grammes": "g",
+    "jours": "jours",
     "kilomètre heure": "kilomètres par heure",
     "kilomètres à l'heure": "kilomètres par heure",
-    "mètres": "m",
-    "minutes": "mn",  # As seen from Snips
-    "d'heures": "en heures",
-    "yards": "yards",
-    "gallons": "gallons",  # "galons"?
-    "litres": "l",
-    "livres": "livres",
-    "grammes": "g",
-    "degrés": "degrés",
-    "fahrenheit": "fahrenheit",
     "kilomètres": "km",
-    "pouces": "pouces",
+    "litres": "l",
+    "livre": "livre",
+    "livres": "livres",
     "mètre": "m",
+    "mètres": "m",
+    "miles": "miles",  # or "mailles"?
+    "miles par heure": "miles à l'heure",
+    "minutes": "mn",  # As seen from Snips
+    "nœuds": "noeuds",
+    "pieds": "pieds",
+    "pintes": "pintes",
+    "pouces": "pouces",
+    "yards": "yards",
 }
 
 
